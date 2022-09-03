868c3b5888c764bfea8242eb60a4541ec79a8fbb
Kollurupranathi <kpranathi009@gmail.com>
2022-09-03 19:15:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import logo from './logo.svg';
-
 import './App.css';
 import Data from "./data.json";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import {useState} from 'react';
+import {Container, Row, Col, Form} from 'react-bootstrap';
 function App() {
+  const [state, setState] = useState("");
   return (
-    <div className="App">
-      <div className="products">
-        {Data.map(prod =>{
+    <Container>
+      <Row className="pt-5">
+      <Col ><em><h2><span style={{color:"#f96d00"}}><strong>G</strong></span><span style={{color:"#155263"}}>roceri</span><span style={{color:"#f96d00"}}><strong>E</strong></span><span style={{color:"#155263"}}>ASE</span></h2><span><h6>shop with ease...</h6></span></em></Col>    
+
+          <Col sm={12} md={6} lg={4} xl={3} className="ms-auto">
+            <Form.Control type="search"  onChange={(e)=>{setState(e.target.value)}} placeholder='Search here...'/>
+          </Col>    
+      </Row>
+      <Row>
+        {Data.filter(item=> item.product.toLowerCase().includes(state)).map(prod =>{
           return(
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" width = "150" height = "150" src={ prod.Image } />
+          <Col sm={12} md={6} lg={4} xl={3} className="p-2">
+            <Card style={{ width: '15rem' }}>
+            <Card.Img variant="top"  height = "150" src={ prod.Image } />
             <Card.Body>
               <Card.Title><>
                 <h4>{ prod.product }</h4>
-              
+                <h6>{ prod.Quantity } </h6>
+                <h6>{ prod.Price }</h6>
               </></Card.Title>
-              <Button variant="primary" >click on</Button>
+              <Button variant="primary" >Add to cart</Button>
             </Card.Body>
           </Card>
-            
+          </Col>
           )
         })}
-      </div>
-     <p>"HEllo world"</p> 
-    </div>
+      </Row>
+    </Container>
     
     
   );
